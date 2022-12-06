0c26cf0078f372408458a89f5492ccb206da7d90
niko <66620705+nikoLua@users.noreply.github.com>
2022-12-06 17:48:09 +0800

    Add files via upload

---
+++ consoleClear/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace consoleClear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Press any key to clear the screen.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("The screen has been cleared.");
+        }
+    }
+}
